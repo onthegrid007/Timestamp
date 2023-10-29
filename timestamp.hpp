3ab5a454a7adf06e84bbed13bd92f805c0a514b5
onthegrid007 <peter.ferranti.dev@gmail.com>
2023-10-29 02:11:28 -0400

    .

--- timestamp.hpp
+++ timestamp.hpp
@@ -1,9 +1,15 @@
+/*
+*   BSD 3-Clause License, see file labled 'LICENSE' for the full License.
+*   Copyright (c) 2023, Peter Ferranti
+*   All rights reserved.
+*/
+
 #ifndef TIMESTAMP_HPP_
 #define TIMESTAMP_HPP_
 
 #include <chrono>
 
-template<typename C = std::chrono::high_resolution_clock, typename T = uint64_t, typename D = std::chrono::duration<T, std::nano>>
+template<typename C = std::chrono::high_resolution_clock, typename T = std::uint64_t, typename D = std::chrono::duration<T, std::nano>>
 class _Timestamp : public std::chrono::time_point<C, D> {
     public:
     typedef C ClockType;
